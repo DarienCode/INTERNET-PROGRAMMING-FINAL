ea5d9b0d6dbdb3a8f61a7a2040edeedeafbe0bb4
DarienCode <darientorok1@gmail.com>
2025-05-08 22:42:56 -0500

    nicolas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(e) {
+    e.preventDefault();
+    if (!name) return;
+
+    fetch('http://localhost:4000/add?name=' + name)
+      .then(r => r.json())
+      .then(d => {
+        setTodos(d);
+        setName('');
+      });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={addTodo}>
+        <input value={name} onChange={e => setName(e.target.value)} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map((t, i) => <li key={i}>{t.name}</li>)}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
